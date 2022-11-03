3304560420fd22e5fde8e069500b43a99aa38ed1
Guy Tordjman <turgibot@gmail.com>
2022-11-03 11:09:42 +0200

    move semantics

--- BackToCPP/BackToCPP.cpp
+++ BackToCPP/BackToCPP.cpp
@@ -7,72 +7,74 @@
 */
 
 #include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-constexpr int a() {
-	return 8;
+
+
+class B {
+
+public:
+	string val;
+	B() = default;
+	B(const string& val) : val(val) {}
+	B(const B& other) {
+		if (*this == other) {
+			return;
+		}
+		this->val = other.val;
+	}
+
+	bool operator== (const B& other) const {
+		return this->val == other.val;
+	}
+
+	B& operator= (B&& other) noexcept {
+		this->val = other.val;
+		return *this;
+	}
+
+};
+
+class A {
+public:
+	A() = default;
+	A(B b) {
+		cout << "A Created" << endl;
+	}
+	void print() {
+		cout << "ptr" << endl;
+	}
+	~A() {
+		cout << "A Destroyed" << endl;
+
+	}
+};
+
+void f(shared_ptr<A> a) {
+	a->print();
 }
-
-
 int main()
 {
-	const int i = 1;
-	//i = 4; //impossible i is const at compile time
-	int x = 4;
-	// const and pointers
-	// it all depends where the const is located in reference to the type:
-	//1. before the type - this means that the value of the referenced variable cant be changed.
-	const int* ptr = &x;
-	//*ptr = 2; 
-	ptr = &i; //the pointer itself can be reassigned
+	B b("hello");
+	//B v = b; //copy because creation
+	//B b2 = string("Bye");
+	//b2 = move(v); // assignment operator
 
-	//2. in the middle - after the type same thing as 1
+	//A a(b);
 
-	int const* ptr2 = &x; // data is const - pointer is not
-	//*ptr2 = 3;
+	 vector<int> v = {1, 2, 3, 4, 5};
+   int cnt;
 
-	int* const ptr1 = &x; // now pointer is const - data is not
-	//ptr1 = &i;
+   cnt = count_if(v.begin(), v.end(), [](int x) {x > 10; });
 
-	// summary - it all referes to where the const is in reference to the *
-	// If const is on the left  of *, data is const
-	// If const is on the right of *, pointer is const
+   cout << "There are " << cnt << " numbers are greater that 3." << endl;
 
-	int j = 4;
-	//now these two are the same and mean that the value cannot be changed using the pointer
-	const int* ptr3 = &j;
-	int const* ptr4 = &j;
-	//*ptr3 = 4;
-
-	// when the const is after the * than the pointer itself is const
-
-	int* const ptr5 = &x; // must be initialized
-	//ptr5 = &j; 
-
-	//now both:
-
-	const int* const both_ptr = &x;
-	//or
-	int const* const both_ptr2 = &x;
-
-	// we can remove the const referece to a value and still change it with a const pointer using const_cast
-	int k = 4;
-	const int* ptr_k = &k;
-	cout << "old value is :" << *ptr_k << endl;
-	// force changing the value using const_cast
-	*(const_cast<int*>(ptr_k)) = 99;
-	cout << "new value is :" << *ptr_k << endl;
-
-	// we can also add constantness
-
-	int l = 400;
-
-	(static_cast<const int&>(l));
+	shared_ptr<A> a_ptr(new A(b));
+	f(a_ptr);
 	
-
-	const int y = 8;
-	int arr[a()];
-
-
 
 }
